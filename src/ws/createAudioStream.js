ecbfd195a8c0303c87e31a4345c65da31ec78377
Brian <brianflaherty64@gmail.com>
2021-03-30 23:14:22 -0400

    feat: createAudioStream now uses audio file data from GraphQL API

--- src/ws/createAudioStream.js
+++ src/ws/createAudioStream.js
@@ -1,4 +1,7 @@
+const get = require('lodash/get');
 const { throwError } = require('rxjs');
+const {map,mergeMap} = require('rxjs/operators');
+const {findAudioFiles} = require('@buccaneerai/graphql-sdk');
 
 const streamS3Audio = require('../operators/streamS3Audio');
 const ingestAudioFromClient = require('./ingestAudioFromClient');
@@ -7,24 +10,33 @@ const errors = {
   audioFileDNE: () => new Error('audio file does not exist'),
 };
 
-const createInputStream = (
+const createInputStream = function createInputStream(
   config,
-  s3Bucket = process.env.S3_AUDIO_BUCKET,
-  s3Prefix = 'demo/'
-) => clientStream$ => {
-  switch (config.inputType) {
-    case 's3File':
-      return streamS3Audio({
-        s3Bucket,
-        s3Key: `${s3Prefix}${config.audioFileId}.linear16`,
-      });
-    case 'audioStream':
-      return clientStream$.pipe(ingestAudioFromClient());
-    // case 'fileUpload':
-    // return clientStream$.pipe();
-    default:
-      return throwError(new Error('unknown input type'));
-  }
+  _findAudioFiles = findAudioFiles
+) {
+  return clientStream$ => {
+    switch (config.inputType) {
+      case 's3File':
+        return _findAudioFiles({filter: {_id: config.audioFileId}}).pipe(
+          map(response => get(response, 'audioFiles[0]', null)),
+          mergeMap(audioFile =>
+            audioFile
+            ? streamS3Audio({
+              s3Bucket: audioFile.s3Bucket,
+              s3Key: audioFile.s3Key,
+              // s3Key: `${s3Prefix}${config.audioFileId}.linear16`,
+            })
+            : throwError(errors.audioFileDNE())
+          )
+        );
+      case 'audioStream':
+        return clientStream$.pipe(ingestAudioFromClient());
+      // case 'fileUpload':
+      // return clientStream$.pipe();
+      default:
+        return throwError(new Error('unknown input type'));
+    }
+  };
 };
 
 module.exports = createInputStream;
