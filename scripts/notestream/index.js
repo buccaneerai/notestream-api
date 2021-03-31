85df4fa9321cbe8adfe52ee92be575ec51606fe9
Brian <brianflaherty64@gmail.com>
2021-03-30 23:15:55 -0400

    feat: added placeholders for notestream scripts

--- scripts/notestream/index.js
+++ scripts/notestream/index.js
@@ -1,20 +1,28 @@
+const dotenv = require('dotenv');
 const get = require('lodash/get');
 const {Command} = require('commander');
 const runPipeline = require('./lib/runPipeline');
 const s3FileToSTT = require('./lib/fileToSTT');
 
 const program = new Command();
+dotenv.config({path: path.resolve(__dirname, '../../.env')});
 
 // node ./bin/code-generators/generateScaffold --help
 program
-  .command('stt <audioFileS3Key>')
+  .command('file2stt <audioFileId>')
   .description('Runs the notestream STT pipeline on an S3 file')
   .option('-d, --debug', 'output extra debugging')
   .option('-b, --bucket', 's3 Bucket', get(process.env, 'S3_DATA_STORAGE_BUCKET', null))
-  .option('-e, --engines [...sttEngines]', 'One or more s3Engines to use', ['gcp'])
-  .action((audioFileS3Key, options) =>
-    runPipeline(s3FileToSTT(audioFileS3Key, options)
-  );
+  .option(
+    '-e, --engines [...sttEngines]',
+    'One or more STT Engines to use from [gcp,aws,awsMed,deepgram]',
+    ['gcp']
+  )
+  .option('--save-audio', 'Boolean: store audio to S3', false)
+  .option('--save-raw-stt', 'Boolean: store raw STT to S3', false)
+  .option('--save-words', 'Boolean: store raw words to S3', false)
+  .option('-a, --secret-token', 'JWT token', get(process.env, 'JWT_TOKEN', null))
+  .action((audioFileId, opts) => runPipeline(s3FileToSTT(audioFileId, opts));
   // .option('--plural-form <pluralForm>', 'the plural form of the resource name (defaults to adding an "s")')
   // .option('--dry-run', 'write output to console instead of files')
   // .option('-w --without', 'model, create, remove, delete, update')
