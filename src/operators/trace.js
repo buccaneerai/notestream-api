5bd3c4632c757105c7efa09bc0c0849a65168ce1
Brian <brianflaherty64@gmail.com>
2022-02-23 13:12:36 -0500

    dev: improved trace logging to make logs less noisy

--- src/operators/trace.js
+++ src/operators/trace.js
@@ -1,6 +1,17 @@
+const isArray = require('lodash/isArray');
+const omit = require('lodash/omit');
 const { tap } = require('rxjs/operators');
 const { info } = require('../utils/logger');
 
-const trace = (label, log = info) => tap(data => log(label, data));
+const defaultOptions = {
+  blacklist: ['context', 'data.context'],
+};
+
+const trace = (label, options = defaultOptions, log = info) => (
+  tap(data => log(
+    label,
+    isArray(data) ? null : omit(data, ...options.blacklist)
+  ))
+);
 
 module.exports = trace;
