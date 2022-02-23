c89d32f5d6fbec4eefbb8d9e32f75d7f39be52da
Brian <brianflaherty64@gmail.com>
2022-02-23 13:19:56 -0500

    tests: unit tests now pass

--- src/operators/trace.test.js
+++ src/operators/trace.test.js
@@ -9,15 +9,15 @@ const trace = require('./trace');
 describe('trace', () => {
   it('should log the data that passes through the observable', observe(() => {
     const logSpy = sinon.spy();
-    const input$ = of(...['foo', 'bar']);
+    const input$ = of(...[{context: 'donotlog', name: 'foo'}, {name: 'bar'}]);
     return input$.pipe(
-      trace('output', logSpy),
+      trace('output', {blacklist: ['context']}, logSpy),
       finalize(() => {
         expect(logSpy.callCount).to.equal(2);
         expect(logSpy.getCall(0).args[0]).to.equal('output');
-        expect(logSpy.getCall(0).args[1]).to.equal('foo');
+        expect(logSpy.getCall(0).args[1]).to.deep.equal({name: 'foo'});
         expect(logSpy.getCall(1).args[0]).to.equal('output');
-        expect(logSpy.getCall(1).args[1]).to.equal('bar');
+        expect(logSpy.getCall(1).args[1]).to.deep.equal({name: 'bar'});
       })
     );
   }));
