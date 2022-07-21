63bd5e7e4f99e47b039d51166b92c2d26bd86afc
Brian <brianflaherty64@gmail.com>
2022-07-21 01:27:38 -0400

    dev: scripts can now stream live audio data and process it

--- scripts/notestream/index.js
+++ scripts/notestream/index.js
@@ -5,6 +5,7 @@ const {Command} = require('commander');
 // const runPipeline = require('./lib/runPipeline');
 // const s3FileToSTT = require('./lib/fileToSTT');
 const testWebsocket = require('./lib/testWebsocket');
+const liveStream = require('./lib/liveStream');
 
 const program = new Command();
 dotenv.config({path: path.resolve(__dirname, '../../.env')});
@@ -33,16 +34,41 @@ dotenv.config({path: path.resolve(__dirname, '../../.env')});
     // generateScaffold({name: resourceName, fields, ...options}))
   // );
 
+// JWT token corresponding to the test Practitioner user
+const fakeUserToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDM1NjVmNzM4MGRmMjAwMzA4YjAyOWMiLCJlbWFpbCI6ImJvbmVzLm1jY295QGJ1Y2NhbmVlci5haSIsImlzQWRtaW4iOmZhbHNlLCJhY2NvdW50SWQiOiI2MjJhNTk4ODRmNzI5YjgxMmI0NjgxZDciLCJyb2xlSWRzIjpbIjYyMmE2MmIzODNiY2MwOTZkYjY2ZGYwZSJdLCJpYXQiOjE2NTgzMzI0MTcsImV4cCI6MjA5MDMzMjQxN30.ygdm2fGzvpi2TOKlkloihxCfRw8fgH4LbRY-mTYN2kc';
+const audioFilePath = path.resolve(
+  __dirname,
+  '../audio-samples/headache-sample.l16'
+);
 program
-  .command('simulate')
+  .command('from-file')
   .option('-f, --audio-file-id <audioFileId>', 'audio file to run', '60621d23347140dc6007dba2')
   .option('-u, --url <url>', 'url of server',`http://localhost:${process.env.PORT}`)
-  .option('-a, --token <token>', 'JWT token', process.env.JWT_TOKEN)
+  .option('-a, --token <token>', 'JWT token', fakeUserToken)
   .option('--stt-engines <sttEngines...>', 'stt engines to use', ['aws-medical', 'gcp', 'ibm'])
-  .option('--ensemblers <ensemblers...>', 'ensemblers to use')
-  .option('--save-words', 'whether to store word output', true)
+  .option('--ensemblers <ensemblers...>', 'ensemblers to use', [])
+  .option('--input-type <inputType>', 'input source type', 'audioFile')
   .option('--save-raw-stt', 'whether to store raw STT ooutput', true)
+  .option('--save-words', 'whether to store word output', true)
   .option('--save-windows', 'whether to save note windows', true)
   .action(opts => testWebsocket(opts))
+
+program
+  .command('from-stream')
+  .option('-u, --url <url>', 'target URL', `http://localhost:${process.env.PORT}`)
+  .option('-t, --token <token>', 'JWT token', fakeUserToken)
+  .option('-i, --input-file-path <inputFilePath>', 'Path to (LINEAR16) audio file', audioFilePath)
+  .option('-e, --stt-engines <sttEngines...>', 'stt engines', ['aws-medical', 'gcp', 'ibm'])
+  .option('--ensemblers <ensemblers...>', 'ensemblers', [])
+  .option('--input-type <inputType>', 'input source type', 'audioStream')
+  .option('--take <take>', 'Number of seconds to sample', null)
+  .option('--skip <skip>', 'Number of seconds to skip', null)
+  // .option('--json', 'Return only the final result (as JSON)', false)
+  .option('--save-raw-stt', 'save raw STT output', false)
+  .option('--save-words', 'save words output', false)
+  .option('--save-windows', 'whether to save note windows', true)
+  .action(opts => liveStream()({...opts})
+    .subscribe(console.log,console.error,console.log.bind(null, 'DONE'))
+  );
 
 program.parse(process.argv);
