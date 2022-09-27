986e063015d4dd7b21c89fb1e1bd916228a72988
Brian <brianflaherty64@gmail.com>
2022-09-26 22:18:29 -0400

    feat: app now updates the status of run when it disconnects, stops, or is completed

--- src/ws/consumeOneClientStream.js
+++ src/ws/consumeOneClientStream.js
@@ -8,16 +8,22 @@ const {
   share,
   shareReplay,
   take,
+  takeLast,
   takeUntil,
   withLatestFrom
 } = require('rxjs/operators');
 
-const { DISCONNECTION, STT_STREAM_STOP } = require('./producer');
+const {
+  DISCONNECTION,
+  STT_STREAM_COMPLETE,
+  STT_STREAM_STOP
+} = require('./producer');
 const getStreamConfig = require('./getStreamConfig');
 const createAudioStream = require('../audio/createAudioStream');
 const toSTT = require('../operators/toSTT');
 const trace = require('../operators/trace');
 const createWindows = require('../operators/createWindows');
+const storeStatusUpdates = require('../storage/storeStatusUpdates');
 // const storeRawAudio = require('../storage/storeRawAudio');
 
 const getSttConfig = config => pick(
@@ -37,11 +43,9 @@ const consumeOneClientStream = function consumeOneClientStream(
   _createAudioStream = createAudioStream,
   _toSTT = toSTT,
   _getStreamConfig = getStreamConfig,
-  // _predictElements = predictElements,
   // _storeRawAudio = storeRawAudio,
-  // _storeNlp = storeNlp,
-  // _createPredictions = createPredictions,
-  _createWindows = createWindows
+  _createWindows = createWindows,
+  _storeStatusUpdates = storeStatusUpdates
 ) {
   return connectionStream$ => {
     const clientStreamSub$ = connectionStream$.pipe(shareReplay(5));
@@ -60,10 +64,10 @@ const consumeOneClientStream = function consumeOneClientStream(
     const config$ = clientStreamSub$.pipe(
       _getStreamConfig(),
       take(1),
+      trace('ws.START_STREAM'),
       shareReplay(1)
     );
     const stt$ = config$.pipe(
-      trace('ws.START_STREAM'),
       withLatestFrom(socket$),
       map(([config, socket]) => [config, get(socket, 'handshake.auth.token')]),
       mergeMap(([config, token]) =>
@@ -83,6 +87,12 @@ const consumeOneClientStream = function consumeOneClientStream(
         )
       ),
       map(event => ({ ...event, pipeline: 'stt' })),
+      takeUntil(end$),
+      share()
+    );
+    const complete$ = stt$.pipe(
+      takeLast(1),
+      map(() => ({type: STT_STREAM_COMPLETE})),
       share()
     );
     const noteWindow$ = config$.pipe(
@@ -90,22 +100,28 @@ const consumeOneClientStream = function consumeOneClientStream(
       filter(words => !words),
       share()
     );
-    // const output$ = combineLatest([config$, noteWindow$]).pipe(
-    //   map(([,noteWindow]) => stt$),
-    //   trace('consumeOneClientStream.out')
-    // );
     const runId$ = config$.pipe(
-      map(conf => ({runId: conf.runId, pipeline: 'runCreated'}))
+      map(conf => ({runId: conf.runId, pipeline: 'runCreated'})),
+      take(1)
+    );
+    // track the status of the run on the MongoDB object
+    const statusUpdate$ = _storeStatusUpdates({config$, end$, complete$}).pipe(
+      filter(() => false) // we don't actually need to return the data
     );
     const output$ = config$.pipe(
+      take(1),
       mergeMap(config => merge(
         runId$,
         noteWindow$,
-        stt$.pipe(filter(() => config.sendSTTOutput))
+        stt$.pipe(filter(() => config.sendSTTOutput)),
+        statusUpdate$
       )),
     );
-    const messageBack$ = combineLatest([socket$, output$]).pipe(
-      takeUntil(end$),
+    const messageBack$ = combineLatest([
+      socket$.pipe(take(1)),
+      output$
+    ]).pipe(
+      // takeUntil(end$),
       map(([socket, event]) => socket.emit('message', event))
     );
     return messageBack$;
