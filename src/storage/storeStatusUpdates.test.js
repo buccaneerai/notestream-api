56bbdbc53b5ee91faad9c38b179d8ae26e067116
Brian <brianflaherty64@gmail.com>
2022-09-26 23:55:49 -0400

    dev(tests): fixed unit tests for status storage

--- src/storage/storeStatusUpdates.test.js
+++ src/storage/storeStatusUpdates.test.js
@@ -1,7 +1,7 @@
 const {expect} = require('chai');
 const sinon = require('sinon');
 const {marbles} = require('rxjs-marbles/mocha');
-const {throwError} = require('rxjs');
+const {of,throwError} = require('rxjs');
 
 const {DISCONNECTION, STT_STREAM_STOP, STT_STREAM_COMPLETE} = require('../ws/producer');
 const storeStatusUpdates = require('./storeStatusUpdates');
@@ -18,11 +18,11 @@ describe('storeStatusUpdates', () => {
 
   it('should attempt retries before erroring out', marbles(m => {
     const events = [
+      {type: STT_STREAM_STOP},
       {type: DISCONNECTION},
-      {type: STT_COMPLETE},
     ];
     const error = new Error('bad response');
-    const updateRun = sinon.stub().returns(m.cold('#', null, error));
+    const updateStatus = sinon.stub().returns(m.cold('#', null, error));
     const end$ = m.cold('--0-1|', events);
     const complete$ = m.cold('-----|', []);
     const config$ = m.cold('-(0|)', [fakeConfig]);
@@ -30,10 +30,7 @@ describe('storeStatusUpdates', () => {
       config$,
       end$,
       complete$,
-      _updateStatus: updateStatus({
-        runId: fakeConfig.runId,
-        _updateRun: () => () => updateRun,
-      }),
+      _updateStatus: () => updateStatus,
       retryConfig: {count: 3, delay: 0},
      });
     const expected$ = m.cold('------------#', null, error);
@@ -42,11 +39,11 @@ describe('storeStatusUpdates', () => {
 
   it('should return stream of updates given valid input', marbles(m => {
     const events = [
-      {type: DISCONNECTION},
       {type: STT_STREAM_STOP},
+      {type: DISCONNECTION},
     ];
     const fakeResponse = {updateRun: {_id: fakeConfig.runId}};
-    const updateRun = sinon.stub().returns(m.cold('(-0|)', [fakeResponse]));
+    const updateStatus = sinon.stub().returns(m.cold('(-0|)', [fakeResponse]));
     const end$ = m.cold('--0-1|', events);
     const complete$ = m.cold('-----|', []);
     const config$ = m.cold('-(0|)', [fakeConfig]);
@@ -54,13 +51,21 @@ describe('storeStatusUpdates', () => {
       config$,
       end$,
       complete$,
-      _updateStatus: updateStatus({
-        runId: fakeConfig.runId,
-        _updateRun: () => () => updateRun,
-      }),
+      _updateStatus: () => updateStatus,
       retryConfig: {count: 3, delay: 0},
      });
     const expected$ = m.cold('---0-1|', [fakeResponse, fakeResponse]);
+    m.expect(actual$).toBeObservable(expected$);
+  }));
+
+  it('.updateStatus should return a function that returns an observable', marbles(m => {
+    const fakeResponse = {updateRun: {_id: fakeConfig.runId}};
+    const updateRun = sinon.stub().returns(of(fakeResponse));
+    const actual$ = updateStatus({
+      runId: 'myrunid',
+      _updateRun: () => updateRun
+    })({type: DISCONNECTION});
+    const expected$ = m.cold('(-0|)', [fakeResponse]);
     m.expect(actual$).toBeObservable(expected$);
   }));
 });
