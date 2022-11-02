f00f91a79159ec1fc3f3223fc470fb7970e498b0
Brian Flaherty <brianflaherty64@gmail.com>
2022-11-02 12:06:16 -0400

    fix(bug): resolved issue where notestream would stop accepting new connections after an error

--- src/ws/createConsumer.js
+++ src/ws/createConsumer.js
@@ -1,5 +1,6 @@
 const omit = require('lodash/omit');
-const { map, mergeAll, tap } = require('rxjs/operators');
+const { EMPTY } = require('rxjs');
+const { catchError, map, mergeAll, tap } = require('rxjs/operators');
 
 const { fromSocketIO, NEXT_AUDIO_CHUNK } = require('./producer');
 const consumeOneClientStream = require('./consumeOneClientStream');
@@ -9,17 +10,24 @@ const createConsumer = ({
   io,
   _fromSocketIO = fromSocketIO,
   _consumeOneClientStream = consumeOneClientStream,
+  _logger = logger
 }) => {
   const clientStream$$ = _fromSocketIO({ io });
   const outputStream$$ = clientStream$$.pipe(
     map(input$ => input$.pipe(
       tap(e =>
         e.type !== NEXT_AUDIO_CHUNK
-        ? logger.info('WS_PRODUCED', omit(e, 'context', 'data.context', 'data.chunk'))
+        ? _logger.info('WS_PRODUCED', omit(e, 'context', 'data.context', 'data.chunk'))
         : null
       ),
     )),
-    map(clientStream$ => clientStream$.pipe(_consumeOneClientStream()))
+    map(clientStream$ => clientStream$.pipe(
+      _consumeOneClientStream(),
+      catchError(err => {
+        _logger.error(err);
+        return EMPTY;
+      })
+    ))
   );
   const consumer$ = outputStream$$.pipe(mergeAll());
   return consumer$;
